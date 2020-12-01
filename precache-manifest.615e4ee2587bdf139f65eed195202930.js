2298791f4d7d871133dd90b6d31e3a7c8cddd612
Aiman Ansari <aiman-ansari@users.noreply.github.com>
2020-12-01 14:57:15 +0530

    Updates

---
+++ precache-manifest.615e4ee2587bdf139f65eed195202930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97adcaeb84ab23a77e9b43d715ccfea0",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "8d97d6fa4ea1d4ae5d70",
+    "url": "/React-todo-list/static/css/main.26a51521.chunk.css"
+  },
+  {
+    "revision": "e260f90ed10d05e2b6b9",
+    "url": "/React-todo-list/static/js/2.5504f8ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list/static/js/2.5504f8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d97d6fa4ea1d4ae5d70",
+    "url": "/React-todo-list/static/js/main.74f7acb1.chunk.js"
+  },
+  {
+    "revision": "96c0dd7e408ab5b7c115",
+    "url": "/React-todo-list/static/js/runtime-main.706fffc7.js"
+  }
+]);
